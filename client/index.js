0480f7b098d048d818a347f922a364a68c63afa4
Murphy Randle <murphyrandle@gmail.com>
2015-08-22 21:57:31 -0600

    Use hash location on the client-only demo app

--- client/index.js
+++ client/index.js
@@ -3,6 +3,10 @@ import Router from 'react-router'
 
 import routes from './routes'
 
-Router.run(routes, Router.HistoryLocation, Root => {
+var locationType = Router.HistoryLocation
+if (location.host.indexOf('non-universal') != -1) {
+  locationType = Router.HashLocation
+}
+Router.run(routes, locationType, Root => {
   React.render(<Root />, document.getElementById('app'))
 })
